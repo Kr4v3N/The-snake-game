421aaafe5e0ef800d565fc1d1da7e5fd9ff6b8ac
Kr4v3N <fafachen37@gmail.com>
2018-02-04 13:04:37 +0100

    Snake took the wall

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function ()//Permet d'executer la fonction quand le chargement de la page est terminé.(fonction anonyme que l'on exécute immédiatement).
+window.onload = function () //Permet d'executer la fonction quand le chargement de la page est terminé.(fonction anonyme que l'on exécute immédiatement).
 {
     var canvasWidth = 900;
     var canvasHeight = 600;
@@ -7,82 +7,84 @@ window.onload = function ()//Permet d'executer la fonction quand le chargement d
     var delay = 100;
     var snakee;
     var applee;
+    var widthInBlocks = canvasWidth / blockSize;
+    var heightInBlocks = canvasHeight / blockSize;
 
-    init();//On exécute la fonction init().
+    init(); //On exécute la fonction init().
 
-    function init ()//Fonction d'initialisation standard.
-    {       
-        var canvas = document.createElement('canvas');/*On crée un élément sur notre page html.*/
-        canvas.width = canvasWidth;//Largeur du canvas.
-        canvas.height = canvasHeight;//Hauteur du canvas.
-        canvas.style.border = "1px solid";//Bordure du canvas.
-        document.body.appendChild(canvas);//On attache canvas à notre page html.
-        ctx = canvas.getContext('2d');//On dessine dans le canvas en deux dimensions.
-        snakee = new Snake([[6,4], [5,4], [4,4]], "right");
-        applee = new Apple([10,10]);
-        refreshCanvas();//La fonction init appelle la fonction refreshConvas().
+    function init() //Fonction d'initialisation standard.
+    {
+        var canvas = document.createElement('canvas'); /*On crée un élément sur notre page html.*/
+        canvas.width = canvasWidth; //Largeur du canvas.
+        canvas.height = canvasHeight; //Hauteur du canvas.
+        canvas.style.border = "1px solid"; //Bordure du canvas.
+        document.body.appendChild(canvas); //On attache canvas à notre page html.
+        ctx = canvas.getContext('2d'); //On dessine dans le canvas en deux dimensions.
+        snakee = new Snake([[6, 4], [5, 4], [4, 4]], "right");
+        applee = new Apple([10, 10]);
+        refreshCanvas(); //La fonction init appelle la fonction refreshConvas().
 
     }
-    function refreshCanvas()//Rafraîchir le canvas.
+
+    function refreshCanvas() //Rafraîchir le canvas.
     {
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight);//Effacer tout le rectangle.
         snakee.advance();
-        snakee.draw();
-        applee.draw();
-        setTimeout(refreshCanvas,delay);/*La methode de setTimeout()appelle une fonction aprés un nombre spécifié de millisecondes(100)en effet , elle permet de dire execute-moi la fonction refreshCanvas à chaque 0.1seconde.*/
-        
+        if (snakee.checkCollision()) {
+            // GAME OVER
+        } else {
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight); /*Effacer tout le rectangle.*/
+            snakee.draw();
+            applee.draw();
+            setTimeout(refreshCanvas, delay); /*La methode de setTimeout()appelle une fonction aprés un nombre spécifié de millisecondes(100)en effet , elle permet de dire execute-moi la fonction refreshCanvas à chaque 0.1seconde.*/
+        }
+
+
     }
-    function drawBlock(ctx, position)
-    {
+
+    function drawBlock(ctx, position) {
         var x = position[0] * blockSize;
         var y = position[1] * blockSize;
-        ctx.fillRect(x ,y , blockSize, blockSize);
+        ctx.fillRect(x, y, blockSize, blockSize);
     }
-    function Snake(body, direction)
-    {
-        this.body = body; 
+
+    function Snake(body, direction) {
+        this.body = body;
         this.direction = direction;
-        this.draw = function()
-        {
+        this.draw = function () {
             ctx.save();
-            ctx.fillStyle = "#ff0000";// Couleur du rectangle.
-            for(var i = 0; i < this.body.length; i++)
-            {
+            ctx.fillStyle = "#ff0000"; // Couleur du rectangle.
+            for (var i = 0; i < this.body.length; i++) {
                 drawBlock(ctx, this.body[i]);
             }
             ctx.restore();
 
         };
-        this.advance = function()
-        {
+        this.advance = function () {
             var nextPosition = this.body[0].slice();
-            switch(this.direction)
-                {                  
-                    case "left":
-                        nextPosition[0] -= 1;
-                        break;
-                    case "right":
-                        nextPosition[0] += 1;
-                        break;
-                    case "down":
-                        nextPosition[1] += 1;
-                        break;
-                    case "up":
-                        nextPosition[1] -= 1;
-                        break;
-                    default:
-                        throw("Invalid Direction");
-                }          
-                
+            switch (this.direction) {
+                case "left":
+                    nextPosition[0] -= 1;
+                    break;
+                case "right":
+                    nextPosition[0] += 1;
+                    break;
+                case "down":
+                    nextPosition[1] += 1;
+                    break;
+                case "up":
+                    nextPosition[1] -= 1;
+                    break;
+                default:
+                    throw ("invalid Direction");
+            }
+
             this.body.unshift(nextPosition);
-            this.body.pop(); 
-};
-        this.setDirection = function(newDirection)
-        {
+            this.body.pop();
+        };
+        this.setDirection = function (newDirection) {
             var allowedDirections;
-            switch(this.direction)
-            {
-                case "left":      
+            switch (this.direction) {
+                case "left":
                 case "right":
                     allowedDirections = ["up", "down"];
                     break;
@@ -91,57 +93,77 @@ window.onload = function ()//Permet d'executer la fonction quand le chargement d
                     allowedDirections = ["left", "right"];
                     break;
                 default:
-                    throw("Invalid Direction");
+                    throw ("invalid Direction");
             }
-            if (allowedDirections.indexOf(newDirection) > -1)
-                {
-                    this.direction = newDirection;
-                }
+            if (allowedDirections.indexOf(newDirection) > -1) {
+                this.direction = newDirection;
+            }
+        };
+        this.checkCollision = function () {
+            var wallCollision = false;
+            var snakeCollision = false;
+            var head = this.body[0];
+            var rest = this.body.slice(1);
+            var snakeX = head[0];
+            var snakeY = head[1];
+            var minX = 0;
+            var minY = 0;
+            var maxX = widthInBlocks - 1;
+            var maxY = heightInBlocks - 1;
+            var isNotBetweenHorizontalWalls = snakeX < minX || snakeX > maxX;
+            var isNotBetweenVerticalWalls = snakeY < minY || snakeY > maxY;
+
+            if (isNotBetweenHorizontalWalls || isNotBetweenVerticalWalls) 
+                wallCollision = true; 
+                
+
+            for (var i = 0; i < rest.length; i++) {
+                if (snakeX === rest[i][0] && snakeY === rest[i][1]) 
+                    snakeCollision = true;
+                
+            }
+            return wallCollision || snakeCollision;
+
         };
     }
-    
-    function Apple(position) 
-        {
-            this.position = position;
-            this.draw = function() 
-            {
-                ctx.save();
-                ctx.fillStyle = "#33cc33";
-                ctx.beginPath();
-                var radius = blockSize/2; 
-                var x = position[0]*blockSize + radius;
-                var y = position[1]*blockSize + radius;
-                ctx.arc(x,y, radius, 0, Math.PI*2, true);
-                ctx.fill();
-                ctx.restore();
-            };
-        }
-    
-    
-                
-    document.onkeydown = function handelKeyDown(e)
-    {
+
+    function Apple(position) {
+        this.position = position;
+        this.draw = function () {
+            ctx.save();
+            ctx.fillStyle = "#33cc33";
+            ctx.beginPath();
+            var radius = blockSize / 2;
+            var x = position[0] * blockSize + radius;
+            var y = position[1] * blockSize + radius;
+            ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+            ctx.fill();
+            ctx.restore();
+        };
+    }
+
+
+
+    document.onkeydown = function handelKeyDown(e) {
         var key = e.keyCode;
         var newDirection;
-        switch(key)
-            {
-                case 37:
-                    newDirection = "left";
-                    break;
-                case 38:
-                    newDirection = "up";
-                    break;
-                case 39:
-                    newDirection = "right";
-                    break;
-                case 40:
-                    newDirection = "down";
-                    break;
-                default:
-                    return;
-            }
+        switch (key) {
+            case 37:
+                newDirection = "left";
+                break;
+            case 38:
+                newDirection = "up";
+                break;
+            case 39:
+                newDirection = "right";
+                break;
+            case 40:
+                newDirection = "down";
+                break;
+            default:
+                return;
+        }
         snakee.setDirection(newDirection);
-    }
+    };
 }
-
 
